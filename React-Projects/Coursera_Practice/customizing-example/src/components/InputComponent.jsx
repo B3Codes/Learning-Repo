afd946e9de22aa4cd522a0f9fa40bf6808077698
b3codes <bhnpratap30@gmail.com>
2024-08-29 18:42:37 +0530

    data

---
+++ React-Projects/Coursera_Practice/customizing-example/src/components/InputComponent.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+
+export default function InputComponent(){
+
+  const [inputText,setInputText] = useState("Hello!");
+
+  const[form, setForm] = useState({
+    firstName: '',
+    lastName:'',
+    email:''
+  });
+ 
+  function handleChange(e){
+    setInputText(e.target.value);
+  }
+  return (
+    <>
+
+    <div className="card" style={{display:"flex", flexDirection:"column",gap:"10px", textAlign:"center", margin:"2em"}}>
+      <h4>Implementing useState Hook</h4>
+      <input value={inputText} onChange={handleChange} />
+      <p>Your're Typing {inputText}</p>
+      <button onClick={()=>setInputText("Hello!")}>
+        Reset
+      </button>
+      </div>
+
+      <div className="card" style={{display:"flex", flexDirection:"column", gap:"10px", textAlign:"center"}}>
+        <h4>Registration form using useState Hook</h4>
+        <label>
+          First Name:
+          <input
+            value={form.firstName}
+            onChange={e => {
+              setForm({
+                ...form,
+                firstName:e.target.value
+              });
+            }}
+            style={{marginLeft:"8px"}}
+          />
+        </label>
+
+        <label>
+          Last Name: 
+          <input
+            value={form.lastName}
+            onChange={e => {
+              setForm({
+                ...form,
+                lastName:e.target.value
+              });
+            }}
+          />
+        </label>
+
+        <label>
+          Email: 
+          <input
+            value={form.email}
+            onChange={e => {
+              setForm({
+                ...form,
+                email: e.target.value
+              });
+            }}  
+          />
+        </label>
+        <p>
+          {form.firstName}{' '}
+          {form.lastName}{' '}
+          ({form.email})
+        </p>
+      </div>
+
+
+      <div className="card" style={{display:"flex", flexDirection:"column", gap:"10px",textAlign:"center"}}>
+          <h4>The useRef hook</h4>  
+          <p>
+            <ul style={{textAlign:"left"}}>
+              <li>We use the useRef hook to access a child element directly.</li>
+              <li>When you invoke the useRef hook, it will return a ref object.</li>
+              <li>The ref object has a property named current.</li>
+            </ul>
+          </p>
+      </div>
+    </>
+  );
+}
